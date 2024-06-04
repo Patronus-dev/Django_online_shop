9459ec60783807052d7a29d19689f9114eaf07e7
Patronus-dev <mehdii.dev@gmail.com>
2024-06-04 21:53:14 +0330

    add: 'create Product app model' added.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.views.generic import *
 
-# Create your views here.
+from .models import Product
+
+
+class ProductListView(ListView):
+    # model = Product
+    queryset = Product.objects.filter(active=True)
+    template_name = 'products/product_list.html'
+    context_object_name = 'products'
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
